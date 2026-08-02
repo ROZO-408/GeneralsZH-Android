2b9cba28e5fe45496a0578bf28eb3c5a118075c3
ROZO-408 <mhmdfaty408@gmail.com>
2026-08-02 19:04:31 +0300

    Update MilesOpenAL.cpp

--- Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
+++ Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
@@ -1,1 +1,57 @@
+#include "MilesOpenAL.h"
 
+#if defined(SAGE_USE_OPENAL)
+
+namespace MilesOpenAL
+{
+
+bool Initialize()
+{
+    return true;
+}
+
+void Shutdown()
+{
+}
+
+MilesSample* CreateSample()
+{
+    return new MilesSample();
+}
+
+void DestroySample(MilesSample* sample)
+{
+    delete sample;
+}
+
+void Play(MilesSample*)
+{
+}
+
+void Stop(MilesSample*)
+{
+}
+
+void Pause(MilesSample*)
+{
+}
+
+void SetVolume(MilesSample*, float)
+{
+}
+
+void SetPan(MilesSample*, float)
+{
+}
+
+void SetLooping(MilesSample*, bool)
+{
+}
+
+void SetPosition(MilesSample*, float, float, float)
+{
+}
+
+}
+
+#endif
