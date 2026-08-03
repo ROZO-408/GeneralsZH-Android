9fad20d783c2af94688bbf1dcff965cfb7c39da2
ROZO-408 <mhmdfaty408@gmail.com>
2026-08-03 06:04:08 +0300

    Update MilesOpenAL.cpp

--- Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
+++ Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
@@ -1,97 +1,115 @@
+#include "MilesStub.h"
 #include "MilesOpenAL.h"
+
+#include <AL/al.h>
+#include <AL/alc.h>
+
+static ALCdevice* g_Device = nullptr;
+static ALCcontext* g_Context = nullptr;
 
 namespace MilesOpenAL
 {
 
 bool Initialize()
 {
+    g_Device = alcOpenDevice(nullptr);
+    if (!g_Device)
+        return false;
+
+    g_Context = alcCreateContext(g_Device, nullptr);
+    if (!g_Context)
+        return false;
+
+    alcMakeContextCurrent(g_Context);
     return true;
 }
 
 void Shutdown()
 {
+    if (g_Context)
+    {
+        alcMakeContextCurrent(nullptr);
+        alcDestroyContext(g_Context);
+        g_Context = nullptr;
+    }
+
+    if (g_Device)
+    {
+        alcCloseDevice(g_Device);
+        g_Device = nullptr;
+    }
 }
 
 MilesSample* CreateSample()
 {
-    MilesSample* sample = new MilesSample();
-
-    alGenSources(1, &sample->source);
-
-    return sample;
+    MilesSample* s = new MilesSample();
+    alGenSources(1, &s->source);
+    return s;
 }
 
-void DestroySample(MilesSample* sample)
+void DestroySample(MilesSample* s)
 {
-    if (!sample)
+    if (!s)
         return;
 
-    if (sample->source)
-        alDeleteSources(1, &sample->source);
+    if (s->source)
+        alDeleteSources(1, &s->source);
 
-    if (sample->buffer)
-        alDeleteBuffers(1, &sample->buffer);
+    if (s->buffer)
+        alDeleteBuffers(1, &s->buffer);
 
-    delete sample;
+    delete s;
 }
 
-void Play(MilesSample* sample)
+void Play(MilesSample* s)
 {
-    if (!sample)
+    if (s)
+        alSourcePlay(s->source);
+}
+
+void Stop(MilesSample* s)
+{
+    if (s)
+        alSourceStop(s->source);
+}
+
+void Pause(MilesSample* s)
+{
+    if (s)
+        alSourcePause(s->source);
+}
+
+void SetVolume(MilesSample* s, float volume)
+{
+    if (!s)
         return;
 
-    alSourcePlay(sample->source);
+    s->volume = volume;
+    alSourcef(s->source, AL_GAIN, volume);
 }
 
-void Stop(MilesSample* sample)
+void SetPan(MilesSample* s, float pan)
 {
-    if (!sample)
+    if (!s)
         return;
 
-    alSourceStop(sample->source);
+    s->pan = pan;
+    alSource3f(s->source, AL_POSITION, pan, 0.0f, 0.0f);
 }
 
-void Pause(MilesSample* sample)
+void SetLooping(MilesSample* s, bool loop)
 {
-    if (!sample)
+    if (!s)
         return;
 
-    alSourcePause(sample->source);
+    s->looping = loop;
+    alSourcei(s->source, AL_LOOPING, loop ? AL_TRUE : AL_FALSE);
 }
 
-void SetVolume(MilesSample* sample, float volume)
+void SetPosition(MilesSample* s, float x, float y, float z)
 {
-    if (!sample)
-        return;
-
-    sample->volume = volume;
-    alSourcef(sample->source, AL_GAIN, volume);
+    if (s)
+        alSource3f(s->source, AL_POSITION, x, y, z);
 }
 
-void SetPan(MilesSample* sample, float pan)
-{
-    if (!sample)
-        return;
-
-    sample->pan = pan;
-    alSource3f(sample->source, AL_POSITION, pan, 0.0f, 0.0f);
 }
-
-void SetLooping(MilesSample* sample, bool loop)
-{
-    if (!sample)
-        return;
-
-    sample->looping = loop;
-    alSourcei(sample->source, AL_LOOPING, loop ? AL_TRUE : AL_FALSE);
-}
-
-void SetPosition(MilesSample* sample, float x, float y, float z)
-{
-    if (!sample)
-        return;
-
-    alSource3f(sample->source, AL_POSITION, x, y, z);
-}
-
-} // namespace MilesOpenAL
