e43a3f30fd0d5896cde36831dcb7669197d3b914
ROZO-408 <mhmdfaty408@gmail.com>
2026-08-03 05:57:28 +0300

    Update MilesOpenAL.cpp

--- Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
+++ Core/Libraries/Source/WWVegas/WWAudio/MilesOpenAL.cpp
@@ -1,40 +1,100 @@
-#include "MilesStub.h"
 #include "MilesOpenAL.h"
 
-// ============================================================================
-// Sample management
-// ============================================================================
-
-HSAMPLE AIL_allocate_sample_handle(HDIGDRIVER)
+namespace MilesOpenAL
 {
-    return MilesOpenAL::CreateSample();
+
+bool Initialize()
+{
+    return true;
 }
 
-void AIL_release_sample_handle(HSAMPLE sample)
-{
-    MilesOpenAL::DestroySample(sample);
-}
-
-void AIL_init_sample(HSAMPLE)
+void Shutdown()
 {
 }
 
-void AIL_start_sample(HSAMPLE sample)
+MilesSample* CreateSample()
 {
-    MilesOpenAL::Play(sample);
+    auto* sample = new MilesSample;
+
+    alGenSources(1, &sample->source);
+
+    return sample;
 }
 
-void AIL_stop_sample(HSAMPLE sample)
+void DestroySample(MilesSample* sample)
 {
-    MilesOpenAL::Stop(sample);
+    if (!sample)
+        return;
+
+    if (sample->source)
+        alDeleteSources(1, &sample->source);
+
+    if (sample->buffer)
+        alDeleteBuffers(1, &sample->buffer);
+
+    delete sample;
 }
 
-void AIL_resume_sample(HSAMPLE sample)
+void Play(MilesSample* sample)
 {
-    MilesOpenAL::Play(sample);
+    if (!sample)
+        return;
+
+    alSourcePlay(sample->source);
 }
 
-void AIL_end_sample(HSAMPLE sample)
+void Stop(MilesSample* sample)
 {
-    MilesOpenAL::Stop(sample);
+    if (!sample)
+        return;
+
+    alSourceStop(sample->source);
+}
+
+void Pause(MilesSample* sample)
+{
+    if (!sample)
+        return;
+
+    alSourcePause(sample->source);
+}
+
+void SetVolume(MilesSample* sample, float volume)
+{
+    if (!sample)
+        return;
+
+    sample->volume = volume;
+
+    alSourcef(sample->source, AL_GAIN, volume);
+}
+
+void SetPan(MilesSample* sample, float pan)
+{
+    if (!sample)
+        return;
+
+    sample->pan = pan;
+
+    alSource3f(sample->source, AL_POSITION, pan, 0.0f, 0.0f);
+}
+
+void SetLooping(MilesSample* sample, bool loop)
+{
+    if (!sample)
+        return;
+
+    sample->looping = loop;
+
+    alSourcei(sample->source, AL_LOOPING, loop ? AL_TRUE : AL_FALSE);
+}
+
+void SetPosition(MilesSample* sample, float x, float y, float z)
+{
+    if (!sample)
+        return;
+
+    alSource3f(sample->source, AL_POSITION, x, y, z);
+}
+
 }
